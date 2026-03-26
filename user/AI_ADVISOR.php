ad75e5baf6c9c39e6ebe4b1fb56dda92ab62cad3
Sharienmae <salarda.sharienmae@ue.edu.ph>
2026-03-26 19:34:01 +0800

    AI

--- user/AI_ADVISOR.php
+++ user/AI_ADVISOR.php
@@ -83,9 +83,11 @@ JS;
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
     />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   </head>
 
-<body>
+<body style="background-color: black;">
     <div class="dashboard">
       <?php include __DIR__ . '/includes/sidebar.php'; ?>
       <!-- MAIN CONTENT -->
